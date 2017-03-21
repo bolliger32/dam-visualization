c76a267309589fb20f800bfbf13d9aa5d2f2fc76
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-03-21 11:52:50 -0700

    made directories

--- NID.R
+++ NID.R
@@ -2,25 +2,21 @@
 ### THIS SCRIPT DOWNLOADS THE NID DAMS DATA SET, CLEANS IT, AND RESAVES IT AS .CSV
 ##################################################################################
 
-### DEFINE YOUR NATEL DIRECTORY
-NATEL <- "C:/Users/Carmen/Dropbox (Stephens Lab)/DS421/Natel"
+### DEFINE YOUR NATEL DIRECTORY - uncomment this when working in Rstudio
+#setwd("~/dam-visualization/scripts")
 
 library(dams)
 library(dplyr)
 
 dams <- get_nid()
+dams_bu <- dams
 
 ### SELECT COLUMNS OF INTEREST - THESE WILL BE NARROWED DOWN LATER
 dams <- dams %>%
-  select(Dam_Name,NID_ID,Longitude,Latitude,River,Owner_Name,Owner_Type,Dam_Type,Primary_Purpose,All_Purposes,Year_Completed,Dam_Length,Dam_Height,Structural_Height,Hydraulic_Height,NID_Height,Max_Discharge,Max_Storage,Normal_Storage,NID_Storage,Surface_Area,Drainage_Area,Volume,Permitting_Authority,State_Reg_Agency, Fed_Owner, Fed_Other, Source_Agency, State)
-
-### CHECK FOR N/A VALUES 
-lapply(dams, function(x) which(is.na(x) ) )
+  select(Dam_Name,NID_ID,Longitude,Latitude,River,Owner_Name,Owner_Type,Dam_Type,Primary_Purpose,All_Purposes,Year_Completed,Dam_Length,Dam_Height,Structural_Height,Hydraulic_Height,NID_Height,Max_Discharge,Max_Storage,Normal_Storage,NID_Storage,Surface_Area,Drainage_Area,Permitting_Authority,State_Reg_Dam,State_Reg_Agency, Fed_Owner, Fed_Operation, Source_Agency, State,Url_Address,Submit_Date)
 
 ### THROW OUT ROWS WITH N/A LAT OR LONG
 dams <- dams[!is.na(dams$Latitude)&!is.na(dams$Longitude),]
 
-### PLOT and SAVE AS .CSV
-plot(dams$Longitude,dams$Latitude)
-setwd(NATEL)
-write.csv(dams, file="dams.csv",row.names = F)
+### SAVE AS .CSV
+write.csv(dams, file="../data/dams.csv",row.names = F)
