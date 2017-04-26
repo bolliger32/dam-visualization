493ca1c462ce5ce604b7d68a4626453298a22c6a
Carmen Tubbesing <ctubbesing@berkeley.edu>
2017-04-26 14:31:13 -0700

    individual state geojsons

--- scripts/CSVtoGeoJSON.R
+++ scripts/CSVtoGeoJSON.R
@@ -14,3 +14,15 @@ plot(dams)
 
 
 writeOGR(dams, '../data/dams.geojson','dams',driver='GeoJSON')
+
+
+### Save individual state files
+for(i in 1:length(unique(dams$State))){
+  dams_state <- dams[dams$State==unique(dams$State)[i],]
+  dams_state$latitude <- as.character(dams_state$latitude)
+  dams_state$longitude <- as.character(dams_state$longitude)
+  dams_state$latitude <- as.numeric(dams_state$latitude)
+  dams_state$longitude <- as.numeric(dams_state$longitude)
+  dams_state <- SpatialPointsDataFrame(coords=dams_state[,c("longitude","latitude")],data=dams_state[,c(1,2,3,6:32)], proj4string = CRS("+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs"))
+  writeOGR(dams_state, paste('../data/dams_',unique(dams$State)[i],'.geojson',sep=""),paste('dams_',unique(dams$State)[i],sep=""),driver='GeoJSON')
+}
